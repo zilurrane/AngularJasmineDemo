db4cdbf72a8a25ed91ea6195a17c3c40e928638a
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2019-08-12 22:25:30 +0530

    Input param tests and added angular service

--- src/app/child/child.component.spec.ts
+++ src/app/child/child.component.spec.ts
@@ -8,18 +8,25 @@ describe('ChildComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChildComponent ]
+      declarations: [ChildComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
-  beforeEach(() => {
+  it('should create', () => {
     fixture = TestBed.createComponent(ChildComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    expect(component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render latitude and longitude passed as input parameter', () => {
+    const fixture = TestBed.createComponent(ChildComponent);
+    component = fixture.componentInstance;
+    component.latitude = 18.578821075179366;
+    component.longitude = 73.73917436547346;
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement
+    expect(compiled.querySelector('#latitude').textContent).toContain('Latitude : 18.578821075179366');
+    expect(compiled.querySelector('#longitude').textContent).toContain('Longitude : 73.73917436547346');
   });
 });
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent implements OnInit {
+
+  @Input() latitude: Number
+  @Input() longitude: Number
 
   constructor() { }
 
